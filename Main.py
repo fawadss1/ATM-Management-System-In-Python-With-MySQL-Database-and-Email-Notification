769571aa194aab7aaa627746a804e27c9545b1a3
Fawad Ali <71928305+fawadss1@users.noreply.github.com>
2020-12-11 20:41:55 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
 try:
-    # import Sign_Up
+    import Sign_Up
     import Login
     import Welcome
 except ImportError:
     pass
-
-# Modules Used in this Project
-# beautifultable
-# mysql-connector-python
