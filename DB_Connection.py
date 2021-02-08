85fc7eeece45d6bbecd2f23a0ff7d9c24035f7e0
Fawad Ali <71928305+fawadss1@users.noreply.github.com>
2021-02-08 23:39:27 +0500

    Update DB_Connection.py

--- DB_Connection.py
+++ DB_Connection.py
@@ -1,24 +1,20 @@
-import mysql.connector as mysql
+try:
+    import mysql.connector as mysql
 
-Database_Name = "atm"
+    Database = "atm"
 
-chk_db = mysql.connect(
-    host="127.0.0.1",
-    user="root",
-    password=""
-)
-f = chk_db.cursor()
-f.execute("SHOW DATABASES")
-DB = f.fetchall()
-if (Database_Name,) in DB:
-    pass
-else:
-    f.execute("CREATE DATABASE " + Database_Name)
-Database = Database_Name
-
-db = mysql.connect(
-    host="127.0.0.1",
-    user="root",
-    password="",
-    database=Database
-)
+    chk_db = mysql.connect(
+        host="127.0.0.1",
+        user="root",
+        password=""
+    )
+    c_db = chk_db.cursor()
+    c_db.execute("CREATE DATABASE IF NOT EXISTS " + Database)
+    db = mysql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="",
+        database=Database
+    )
+except:
+    print("Sorry! Your DataBase Server is Down! Please Run Your Database Server")
